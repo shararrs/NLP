11c6797001416ea750caf291d931eb4833dbbe65
shararrs <46082441+shararrs@users.noreply.github.com>
2022-09-24 04:11:55 -0500

    Text preprocessing functionality added

--- Portfolio_Assignment_3/word_guess.py
+++ Portfolio_Assignment_3/word_guess.py
@@ -1,11 +1,40 @@
 import sys
 import nltk
-from nltk import word_tokenize
+from nltk import word_tokenize, WordNetLemmatizer, pos_tag
 from nltk.corpus import stopwords
 
 
-def print_hi(name):
-    print(f'Hi, {name}')
+def process_text(tokenized_inp):
+    # readin file like this to keep entire text in memory as 1 line
+    # easier to word tokenize
+
+    # filtered stop, <5 and is alpha
+    game_file = [t for t in tokenized_inp if t.isalpha() and
+                 t not in stopwords.words('english') and len(t) > 5]
+    step_a = len(game_file)
+    # print("all words - stop 11043")
+    # lemmatizing and finding unique set of words
+    # print(f'filtered stop, <5 and is alpha {len(game_file)}')
+    # lemmatizing
+    wnl = WordNetLemmatizer()
+    game_file = [wnl.lemmatize(t) for t in game_file]
+    # create unique list of words
+    game_file = set(game_file)
+    all_unique_words = len(game_file)
+    # print(f'lemmatized unique set {len(game_file)}')
+    # part of speech tagging on unique lemmas
+    game_file = pos_tag(game_file)
+    print('printing first 20 pos tagged words')
+    print(game_file[:20])  # keep
+
+    # List of pos tagged nouns NN only
+    game_file_nn = [t for t in game_file if (t[1] == "NN")]
+    # print(game_file_nn)
+    #print(f'total words after applying filters isalpha, removing stop words and length > 5 are {step_a}')
+    print(len(game_file_nn))
+
+    print(game_file_nn)
+    return game_file_nn
 
 
 # ______________________________________________
@@ -18,11 +47,11 @@ if __name__ == '__main__':
         exit(0)
 
     print(f'You are using the {sys.argv[1]} to play the game')
-    # readin file like this to keep entire text in memory as 1 line
-    # easier to word tokenize
+
+
     inp_file = open(sys.argv[1]).read()
     # tokenizing all words
-    game_file = word_tokenize(inp_file)
+    tokenized_inp = word_tokenize(inp_file)
     # -------------------------------------
     # getting unique words using set()
     # game_file = set(game_file)
@@ -33,13 +62,14 @@ if __name__ == '__main__':
     # print("total unique words - stop words 2819")
     # print(len(game_file))
     # -------------------------------------
-    game_file = [t for t in game_file if t.isalpha() and
-                 t not in stopwords.words('english') and len(t) > 5]
-    print("all words - stop 11043")
-    #lemmatizing and finding unique set of words
-    #game_file =
-    print(len(game_file))
-    game_file = set(game_file)
-    print(len(game_file))
+
+    # lexical diversity
+    print("\nLexical diversity before any pre processing: %.2f" % (len(set(tokenized_inp)) / len(tokenized_inp)))
+
+    text_for_game = process_text(tokenized_inp)
+    #print("After process")
+    #print(text_for_game)
+    #print(len(text_for_game))
+
 
     # inp_file.close()
